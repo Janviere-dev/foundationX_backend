5bd27851745c9f729bae9c5da5eecf2eaa5062f8
BodeMurairi2 <b.murairi@alustudent.com>
2026-07-28 14:49:23 +0200

    feat: wire chat router into the app

--- core/routers.py
+++ core/routers.py
@@ -3,7 +3,9 @@
 from fastapi import FastAPI
 from routers.content_agent_router import router as content_router
 from routers.assessment_router import router as assessment_router
+from routers.chat import router as chat_router
 
 def routers(app:FastAPI):
     app.include_router(content_router)
     app.include_router(assessment_router)
+    app.include_router(chat_router)
