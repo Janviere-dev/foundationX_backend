c99b4aa686228997737a0df7ce70a4dbda5db6ad
BodeMurairi2 <b.murairi@alustudent.com>
2026-07-29 11:21:56 +0200

    feat: add CORS middleware

---
+++ middleware/cors_middleware.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def cors_middleware(app:FastAPI):
+    origins = [
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        )
