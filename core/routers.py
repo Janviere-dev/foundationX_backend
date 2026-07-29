269ee2d90a5874fd20bff49c1adcb10c9368c9ab
BodeMurairi2 <b.murairi@alustudent.com>
2026-07-29 11:21:56 +0200

    feat: wire courses router into the app

--- core/routers.py
+++ core/routers.py
@@ -5,9 +5,11 @@ from routers.content_agent_router import router as content_router
 from routers.assessment_router import router as assessment_router
 from routers.chat import router as chat_router
 from routers.user_router import router as user_router
+from routers.courses_router import router as course_router
 
 def routers(app:FastAPI):
     app.include_router(content_router)
     app.include_router(assessment_router)
     app.include_router(chat_router)
     app.include_router(user_router)
+    app.include_router(course_router)
